bbb815288126e704463416cb61609cc69abc7c92
SantanaAndres <pipe161102@outlook.com>
2026-04-19 12:44:35 -0500

    Just messing around the Spectre.Console utility

---
+++ CLI Multi Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using CLI_Multi_Tool;
+using Spectre.Console;
+
+// Console.WriteLine("En que directorio desea buscar el archivo");
+
+// var userInput = Console.ReadLine();
+
+Repo repo = new Repo();
+
+AnsiConsole.MarkupLine("[green]✓ Build completed successfully[/]");
+
+Console.WriteLine(repo.Search());
