65c839d90ddb749abde872b0e956838eb50486da
SantanaAndres <pipe161102@outlook.com>
2026-05-06 19:32:16 -0500

    Solo se agrego el servicio simple sin validacion ya que de momento al menos no la requiere

---
+++ Aplications/Services/RequestService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Interfaces.Repository;
+using Core.Interfaces.Services;
+
+namespace Aplication.Services;
+
+public class RequestService(IRequestRepo repo): IRequestServices
+{
+    public async Task CheckBTCPrice()
+    {
+        await repo.CheckBTCPrice();
+    }
+}
